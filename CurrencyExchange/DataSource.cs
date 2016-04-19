fd2f669d935abb5863d345ddad832c916479dbdf
Vladas Zakrevskis <146100@gmail.com>
2016-04-19 19:59:13 +0300

    checked banks id

--- CurrencyExchange/DataSource.cs
+++ CurrencyExchange/DataSource.cs
@@ -1,9 +1,11 @@
 ﻿using CurrencyExchange.Entities;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace CurrencyExchange
 {
@@ -11,23 +13,45 @@ namespace CurrencyExchange
     {
         static DataSource()
         {
-            DbContext.Currencies.AddRange(new List<Currency>() { new Currency() { Id = 0, Name = "USD" },
-                                                                 new Currency() { Id = 1, Name = "EUR" },
-                                                                 new Currency() { Id = 2, Name = "RUR" } });
+            //DbContext.Currencies.AddRange(new List<Currency>() { new Currency() { Id = 0, Name = "USD" },
+            //                                                     new Currency() { Id = 1, Name = "EUR" },
+            //                                                     new Currency() { Id = 2, Name = "RUR" } });
 
-            DbContext.Banks.AddRange(new List<Bank>() {  new Bank() { Id = 0,
-                                                                      Name = "Идея Банк",
-                                                                      Address = "г.Минск, ул. З.Бядули, д. 11",
-                                                                      OpenTime = new TimeSpan(10, 00, 00),
-                                                                      CloseTime = new TimeSpan(19, 00, 00) }});
+            //DbContext.Banks.AddRange(new List<Bank>() {  new Bank() { Id = 0,
+            //                                                          Name = "Идея Банк",
+            //                                                          Address = "г.Минск, ул. З.Бядули, д. 11",
+            //                                                          OpenTime = new TimeSpan(10, 00, 00),
+            //                                                          CloseTime = new TimeSpan(19, 00, 00) }});
             
         }
 
         public static CurrencyDbContext DbContext { get; } = new CurrencyDbContext();
 
-        public static string getBankName(int bankId)
+        private static string CurrencySourceXMLPath = @"http://www.obmennik.by/xml/kurs.xml";
+
+        private static void loadCurrencyData()
         {
-            switch (bankId)
+            XDocument doc = XDocument.Load(CurrencySourceXMLPath);
+
+            IEnumerable<XElement> banks = doc.Element("banks").Elements();
+
+            int i = 0;
+
+            foreach(XElement bank in banks)
+            {
+                Console.WriteLine(" " + getBankName(bank.Element("idbank").Value) + " " +
+                     bank.Element("usd").Element("sell").Value + " " + bank.Element("usd").Element("buy").Value) ;
+            }
+
+
+
+        }
+
+        public static string getBankName(string bankId)
+        {
+            int _bankId = int.Parse(bankId);
+
+            switch (_bankId)
             {
                 case 1:  return "Абсолютбанк";
                 case 2:  return "Альфа-Банк";
@@ -61,8 +85,14 @@ namespace CurrencyExchange
                 case 30: return "ТК Банк";
                 case 31: return "Онербанк";
                 case 32: return "Бит-Банк";
+                case 34: return "Н.Е.Б. Банк";
                 default: throw new FormatException("Invalid bank Id");
             }
+        }
+
+        static void Main(string[] args)
+        {
+            loadCurrencyData();
         }
 
 
